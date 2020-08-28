e8ec6c275efe0379ab7199291e0df6517774e596
Selin Akbuğa <selinakbuga@sakbuga.local>
2020-08-28 14:32:38 +0300

    kickstart duzenlemeleri yapıldı.

--- kickstart/kickstart.sh
+++ kickstart/kickstart.sh
@@ -67,11 +67,5 @@ id_rsa_create
 #####authorized_keys kontrol edin, paylaşılan ssh anahtarlarını ekleyin
 authorized_keys_kontrol
 
-#chmod 700 ~/.ssh/authorized_keys
-#cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-#echo -e "\e[93m [kickstart : id_rsa anahtarı authorized keys dosyasına eklendi. --[başarılı] ] ********************** \e[0m"
-#chmod 400 ~/.ssh/authorized_keys
-#chmod 700 ~/.ssh/
-
 machine_reboot
 
